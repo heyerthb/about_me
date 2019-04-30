ab05bb65890b2a3dde03d6f29a4d4d306c495fc9
heyerthb <heyerthb@gmail.com>
2019-04-30 16:19:28 -0700

    initial file setup

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var sandwichName = prompt('HEY KID!!! What is your favorite sandwich?');
+        
+alert(sandwichName +'? Really?? ' + sandwichName + ' is gross!!');
+var yourName = prompt("What's your name " + sandwichName + " sandwich?");
+alert("you're kinda creepy " + yourName + ".");
+var favoriteDrink = prompt("Ok " + yourName + ". Along with your " + sandwichName + " sandwich, what do you like to drink?");
+alert( favoriteDrink + "? It's hard to believe I thought we could make this work " + yourName + ". Between your preference of " + sandwichName + " sandwiches that you wash down with " + favoriteDrink + "; I'm not sure it's going to work out between us.");
+var isItTrue = prompt("One last question..." + yourName + " of the " + sandwichName + " sandwich, who inexplicably drinks " + favoriteDrink +". Is it true what they say?"); 
+alert("I believe you. Alas, two ships passing in the night I guess " + yourName + ". Enjoy your " + sandwichName + " sandwich and warm " + favoriteDrink + " on your long journey. To answer the question you must be asking yourself..." + isItTrue);
+console.log ('The user said thier favorite sandwich is ' + sandwichName + '. Thier favorite drink is ' + favoriteDrink + '. Thier name is ' + yourName + '. And when asked it it were true they responded by saying "' + isItTrue + '".');                   
