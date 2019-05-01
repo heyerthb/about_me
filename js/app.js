ad41f6989f925b54603f64d576daf603e348c3f7
heyerthb <heyerthb@gmail.com>
2019-05-01 14:47:04 -0700

    Lab 2 questions complete. starting Lab 3

--- js/app.js
+++ js/app.js
@@ -1,68 +1,128 @@
-// 'use strict:'
-// var sandwichName = prompt('HEY KID!!! What is your favorite sandwich?');
+// 'use strict';
+
+var userName=prompt('Welcome! My name is Matt, what\'s your name?');
+    alert('Hi ' + userName + '-town! I\'m Matt. Welcome to ME! Let\'s get weird with it. Here\'s a guessing game about me that probably says more about you');
+console.log('The user\'s name is ' + userName + 'alaya!');
+
+// var answerOne=prompt('Guess this about me: Did I have a class in high school where all we did was surf?')
+// .toLowerCase();
+// if(answerOne === 'y' || answerOne ==='yes'){
+//     alert ('You\'re correct ' + userName + 'money! Take a dab. We spent 0 period three days a week surfing before school for PE credit. #blessed')
+// console.log(userName + '-money got the correct answer to #1!')
+// }else{
+//     alert('You lose ' + userName + '! My childhood was basically an episode of the OC, which they largely filmed in my hometown of Hermosa Beach, CA' + '! This is going to be a long game for you.');
+// console.log(userName + '-town is bad and should feel bad');
+// }
+
+// var answerTwo=prompt('You\'re on a roll ' + userName + '-deezy. Yes or no,\; my dogs are named after my favorite US Presidents?')
+// .toLowerCase();
+// if(answerTwo === 'n' || answerTwo === 'no'){
+//     alert(userName + 'You\'re smarter than your face would have me believe ' + userName + ', impressive. I have two Aussie Shepherds named Woodford and Cider respectively after mine and my wife\'s preferred adult beverages.')
+// console.log(userName + ' is so very, very, wrong about answerTwo')
+// }else{
+//     alert(userName + '... take a moment to imagine an entire stadium booing you. You\'re very wrong. Woodford and Cider are their names: named after alcoholic beverages respectively.')
+//     console.log(userName + ' is kinda brilliant right?')
+// }
+
+// var answerThree=prompt('We going next level on this mess ' + userName + '. I am a native Hawaiian?')
+// .toLowerCase();
+// if(answerThree === 'y' || answerThree === 'Yes'){
+//     alert(userName + ", it\'s like you\'re not even trying. WRONG!!! While I have six generations going back on Maui, my blood quantum does not contain Hawaiian blood. I am Filipino-Puerto Rican-Russian")
+// console.log(userName + ' fails hard.')
+// }else{
+//     alert('GET OUT OF MY HEAD ' + userName + '!!! I am not but I identify closely with my Hawaiian family. I have six generations going back on Maui, my blood quantum does not contain Hawaiian blood. I am Filipino-Puerto Rican-Russian')
+//     console.log(userName + ' for the win!! but not actually')
+// }
+
+var answerFour=prompt('Ok, Ok. Humble brag time ' + userName + '. Can you believe that Russel Wilson knows who I am?')
+.toLowerCase();
+if(answerFour === 'y' || answerFour === 'Yes'){
+    alert('Mic drop. Yes he does. When he was drafted by the Hawks he came into my bar. Even after I moved restaurants, whenever I saw him he remembered my name. He\'s good people')
+console.log(userName + ' believes in my truth and I am grateful.')
+}else{
+    alert('I said \'Humble Brag\', you\'d think that would clue you in ' + userName + '. When he was drafted by the Hawks he came into my bar. Even after I moved restaurants, whenever I saw him he remembered my name. He\'s good people')
+    console.log(userName + ' for the win!! but not actually, but closer!')
+}
+
+var answerFive=prompt('Hey ' + userName + ', do you want to hear me sing?')
+.toLowerCase();
+if(answerFive === 'y' || answerFive === 'Yes'){
+    alert('Well ' + userName + ', I may literally have the worst singing voice in this whole wide world. Why are you trying to embarass me? Rude.')
+console.log(userName + ' knows shockingly little about me.')
+}else{
+    alert(userName + ". On a serious note, you may have saved your life and everybody around you. You my friend, are a saint.")
+    console.log(userName + ' is not a fan of my singing voice.')
+}
+
+
+
+
+// // var sandwichName = prompt('HEY KID!!! What is your favorite sandwich?');
         
-// alert(sandwichName +'? Really?? ' + sandwichName + ' is gross!!');
-// // var yourName = prompt("What's your name " + sandwichName + " sandwich?");
-// // alert("you're kinda creepy " + yourName + ".");
-// // var favoriteDrink = prompt("Ok " + yourName + ". Along with your " + sandwichName + " sandwich, what do you like to drink?");
-// // alert( favoriteDrink + "? It's hard to believe I thought we could make this work " + yourName + ". Between your preference of " + sandwichName + " sandwiches that you wash down with " + favoriteDrink + "; I'm not sure it's going to work out between us.");
-// // var isItTrue = prompt("One last question..." + yourName + " of the " + sandwichName + " sandwich, who inexplicably drinks " + favoriteDrink +". Is it true what they say?"); 
-// // alert("I believe you. Alas, two ships passing in the night I guess " + yourName + ". Enjoy your " + sandwichName + " sandwich and warm " + favoriteDrink + " on your long journey. To answer the question you must be asking yourself..." + isItTrue);
-// // console.log ('The user said thier favorite sandwich is ' + sandwichName + '. Thier favorite drink is ' + favoriteDrink + '. Thier name is ' + yourName + '. And when asked it it were true they responded by saying "' + isItTrue + '".');               
+// // alert(sandwichName +'? Really?? ' + sandwichName + ' is gross!!');
+// // // var yourName = prompt("What's your name " + sandwichName + " sandwich?");
+// // // alert("you're kinda creepy " + yourName + ".");
+// // // var favoriteDrink = prompt("Ok " + yourName + ". Along with your " + sandwichName + " sandwich, what do you like to drink?");
+// // // alert( favoriteDrink + "? It's hard to believe I thought we could make this work " + yourName + ". Between your preference of " + sandwichName + " sandwiches that you wash down with " + favoriteDrink + "; I'm not sure it's going to work out between us.");
+// // // var isItTrue = prompt("One last question..." + yourName + " of the " + sandwichName + " sandwich, who inexplicably drinks " + favoriteDrink +". Is it true what they say?"); 
+// // // alert("I believe you. Alas, two ships passing in the night I guess " + yourName + ". Enjoy your " + sandwichName + " sandwich and warm " + favoriteDrink + " on your long journey. To answer the question you must be asking yourself..." + isItTrue);
+// // // console.log ('The user said thier favorite sandwich is ' + sandwichName + '. Thier favorite drink is ' + favoriteDrink + '. Thier name is ' + yourName + '. And when asked it it were true they responded by saying "' + isItTrue + '".');               
 
 
-// // I need 5 yes/no questions about myself. 
-// // 1. My first job was as a butler at the Playboy Mansion. No, it was my fourth. No follow up questions, they make you sign an NDA.
-// // 2. One of my courses in highschool was surf class. Yes, we met at the beach to go surfing three times a week for 0 Period for PE credit. 
-// // 3. My primary motivation for attending law school was rage. Yes. 
-// // 4. My dogs names are Woodford and Cider. Yes, named after my wife and I's drinks of choice.
-// // 5. I 
+// // // I need 5 yes/no questions about myself. 
+// // // 1. My first job was as a butler at the Playboy Mansion. No, it was my fourth. No follow up questions, they make you sign an NDA.
+// // // 2. One of my courses in highschool was surf class. Yes, we met at the beach to go surfing three times a week for 0 Period for PE credit. 
+// // // 3. My primary motivation for attending law school was rage. Yes. 
+// // // 4. My dogs names are Woodford and Cider. Yes, named after my wife and I's drinks of choice.
+// // // 5. I 
 
 
-// var createGreeting = function (username) {
-//     // var hour = 21;
-//     var date = new Date();
-//     var hour = date.getHours()
-//   // if it's noon or later, and before 5 pm, construct "Good afternoon, Stephanie"
-//     if (hour > 0 && hour < 12) {
-// //       var newGreeting = "Good morning, " + username + "!";
-// //     } else if (hour >= 12 && hour < 17) {
-// //       var newGreeting = "Good afternoon, " + username + "!";
-//     } else {
-//       var newGreeting = "Good night, " + username + "!";    
-//     }
+// // var createGreeting = function (username) {
+// //     // var hour = 21;
+// //     var date = new Date();
+// //     var hour = date.getHours()
+// //   // if it's noon or later, and before 5 pm, construct "Good afternoon, Stephanie"
+// //     if (hour > 0 && hour < 12) {
+// // //       var newGreeting = "Good morning, " + username + "!";
+// // //     } else if (hour >= 12 && hour < 17) {
+// // //       var newGreeting = "Good afternoon, " + username + "!";
+// //     } else {
+// //       var newGreeting = "Good night, " + username + "!";    
+// //     }
   
-//     return newGreeting;
-//   };
+// //     return newGreeting;
+// //   };
   
-//   var greeting = createGreeting("Josh");
-//   console.log(greeting); 
+// //   var greeting = createGreeting("Josh");
+// //   console.log(greeting); 
 
-var surfClass = prompt('One of my courses in highschool was surf class?').toLowerCase();
-if (surfClass === 'true'){
-    alert ('We spent 0 period three days a week surfing before school of PE credit. #blessed');
-}
-else if (surfClass === 'false') {
-     alert('Incorrect! My childhood was basically an episode of the OC, which they largely filmed in my hometown of Hermosa Beach, CA');
-}
-else {
-     surfClass= alert('must return either TRUE, or FALSE');
-     prompt('One of my courses in highschool was surf class?');
-}
-console.log ('User believes that is ' + surfClass + ' that I had surf class in high school' )
+// // var surfClass = prompt('One of my courses in highschool was surf class?').toLowerCase();
+// // if (surfClass === 'true'){
+// //     alert ('We spent 0 period three days a week surfing before school of PE credit. #blessed');
+// // }
+// // else if (surfClass === 'false') {
+// //      alert('Incorrect! My childhood was basically an episode of the OC, which they largely filmed in my hometown of Hermosa Beach, CA');
+// // }
+// // else {
+// //      surfClass= alert('must return either TRUE, or FALSE');
+// // //     //  prompt('One of my courses in highschool was surf class?');
+// //     while (surfClass !== 'true' surfClass !== 'false') {'must reutrn either TRUE or FALSE'
+// //     prompt('One of my courses in highschool was surf class?').toLowerCase();
+// // }
+// // console.log ('User believes that is ' + surfClass + ' that I had surf class in high school' )
 
-// var dogNames = prompt('My wife and I named our dogs after our preferred alocoholic beverages?').toLowerCase();
-// if (dogNames === 'true'){
-//     alert ('We spent 0 period three days a week surfing before school of PE credit. #blessed');
-// }
-// else if (surfClass === 'no') {
-//      alert('Incorrect! My childhood was basically an episode of the OC, which they largely filmed in my hometown of Hermosa Beach, CA');
-// }
-// else {
-//      surfClass= alert('must return either YES, or NO');
+// // var dogNames = prompt('My wife and I named our dogs after our preferred alocoholic beverages?').toLowerCase();
+// // if (dogNames === 'true'){
+// //     alert ('We spent 0 period three days a week surfing before school of PE credit. #blessed');
+// // }
+// // else if (surfClass === 'no') {
+// //      alert('Incorrect! My childhood was basically an episode of the OC, which they largely filmed in my hometown of Hermosa Beach, CA');
+// // }
+// // else {
+// //      surfClass= alert('must return either YES, or NO');
 
 
 
 
 
-//  else {'must return an answer of \'\true'\'\false'\'\yes'\'\no'}
+// //  else {'must return an answer of \'\true'\'\false'\'\yes'\'\no'}
